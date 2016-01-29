be2b7fbd97c7b50675628f61b877cfa5086b79f3
Eduardo Baltazar <eduardo.baltazar@hotmail.com>
2016-01-29 16:49:45 -0200

    first commit

---
+++ Portifolio/Site/App/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular
+    .module("website", ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when("/", { templateUrl: "Views/Conhecimentos.html", controller: "Conhecimentos" })
+        .when("/conhecimentos", { templateUrl: "Views/Conhecimentos.html", controller: "Conhecimentos" })
+        .when("/eventos", { templateUrl: "Views/Eventos.html", controller: "Eventos" })
+        .when("/experiencias", { templateUrl: "Views/Experiencias.html", controller: "Experiencias" })
+        .when("/formacoes", { templateUrl: "Views/Formacoes.html", controller: "Formacoes" })
+        .otherwise("/404", { templateUrl: "templates/404.html" });
+}]);
